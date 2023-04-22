98094b8c4c5b0704b8235e2a08fa88381de5cc5d
Andrew Hoog <ahoog42@gmail.com>
2023-04-22 16:23:36 -0500

    pull menuItems from API and update tableView

--- No TLS/Controller/ViewController.swift
+++ No TLS/Controller/ViewController.swift
@@ -7,32 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController, MenuItemManagerDelegate  {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MenuItemManagerDelegate  {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "menuItemCell", for: indexPath)
+        
+        //Default Content Configuration
+        var content = cell.defaultContentConfiguration()
+        content.text = menuItems[indexPath.item]
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         menuItemManager.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     var menuItems: [String] = []
     var itemsString: String = ""
     var menuItemManager = MenuItemManager()
     @IBOutlet weak var menuItemLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     func didUpdateMenuItemData(_ menuItemManager: MenuItemManager, _ menuItemsModel: MenuItemModel) {
         DispatchQueue.main.async {
-            self.menuItemLabel.text = "Updating..."
             menuItemsModel.menuItems.forEach { menuItem in
                 print("menuItem in ViewController didUpdateMenuItemData: \(menuItem)")
                 self.itemsString += "\(menuItem)\r"
                 self.menuItems.append(menuItem)
             }
-            self.menuItemLabel.text = self.itemsString
+            self.tableView.reloadData()
         }
     }
     
     @IBAction func fetchInsecurePressed(_ sender: Any) {
-        menuItemLabel.text = "Processing..."
         menuItemManager.performRequest()
     }
     
