e91117e415827f1088a97214cf931c73b54456be
Andrew Hoog <ahoog42@gmail.com>
2023-04-26 07:24:14 -0700

    add location tracking, user profile controller

--- No TLS/Controller/ViewController.swift
+++ No TLS/Controller/ViewController.swift
@@ -6,8 +6,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MenuItemManagerDelegate  {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate, MenuItemManagerDelegate  {
+    
+    var menuItems: [String] = []
+    var menuItemManager = MenuItemManager()
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var menuItemLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+//        locationManager.requestLocation()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        menuItemManager.delegate = self
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("Got Location Data")
+            locationManager.startUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            print("lat: \(lat)")
+            print("lon: \(lon)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting locations")
+        print(error)
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return menuItems.count
@@ -24,19 +62,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        menuItemManager.delegate = self
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
-    
-    var menuItems: [String] = []
-    var menuItemManager = MenuItemManager()
-    @IBOutlet weak var menuItemLabel: UILabel!
-    @IBOutlet weak var tableView: UITableView!
-    
     func didUpdateMenuItemData(_ menuItemManager: MenuItemManager, _ menuItemsModel: MenuItemModel) {
         DispatchQueue.main.async {
             self.menuItems = []
@@ -52,8 +77,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         menuItemManager.performRequest()
     }
     
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+    
+
+    @IBAction func personPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "gotoProfile", sender: self)
+    }
+    
     func didFailWithError(error: Error) {
         print(error)
     }
-}
+    
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "gotoProfile" {
+            let destinationVC = segue.destination as! ProfileViewController
+        }
+    }
+}
